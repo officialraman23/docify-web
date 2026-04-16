ae1c7e174a9fab4281b42fbbc7ed013e41757d4d
Rajpal Singh <ramansmalsar4@gmail.com>
2026-04-16 01:41:47 -0700

    Add Google Ads tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,19 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16745925913"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16745925913');
+            `,
+          }}
+        />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
